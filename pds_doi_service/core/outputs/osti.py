c8635fb90792af22b49f321e3edb720f26f7b6fa
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-11-13 15:45:06 -0500

    simplify code following resource file re-organization

--- pds_doi_service/core/outputs/osti.py
+++ pds_doi_service/core/outputs/osti.py
@@ -15,7 +15,8 @@ class DOIOutputOsti:
     def __init__(self):
         """Creates a new DOIOutputOsti instance"""
         # Need to find mustache templates relative to root install dir
-        self._root_dir = abspath(join(dirname(__file__), os.pardir, os.pardir, os.pardir))
+        self._draft_template_path = join(dirname(__file__), 'DOI_template_20200407-mustache.xml')
+        self._reserve_template_path = join(dirname(__file__), 'DOI_IAD2_reserved_template_20200205-mustache.xml')
 
     def create_osti_doi_draft_record(self, doi: Doi):
         # The format of 'publication_date' should match the input to 'release' action.
@@ -23,8 +24,7 @@ class DOIOutputOsti:
         doi_fields['publication_date'] = doi.publication_date.strftime('%Y-%m-%d')
         doi_fields['keywords'] = "; ".join(doi.keywords)
         renderer = pystache.Renderer()
-        template_path = join(dirname(__file__), 'DOI_template_20200407-mustache.xml')
-        return renderer.render_path(template_path, doi_fields)
+        return renderer.render_path(self._draft_template_path, doi_fields)
 
     def create_osti_doi_reserved_record(self, dois: list):
         doi_fields_list = []
@@ -38,8 +38,8 @@ class DOIOutputOsti:
             doi_fields_list.append(doi_fields)
 
         renderer = pystache.Renderer()
-        template_path = join(dirname(__file__), 'DOI_IAD2_reserved_template_20200205-mustache.xml')
-        return renderer.render_path(template_path, {'dois': doi_fields_list})
+
+        return renderer.render_path(self._reserve_template_path, {'dois': doi_fields_list})
 
     def create_osti_doi_release_record(self, doi: Doi):
         doi_fields = copy.copy(doi.__dict__)
@@ -50,5 +50,4 @@ class DOIOutputOsti:
         else:
             doi_fields['date_record_added'] = datetime.date.today().strftime('%Y-%m-%d')
 
-        template_path = join(dirname(__file__), 'DOI_template_20200407-mustache.xml')
-        return renderer.render_path(template_path, doi_fields)
+        return renderer.render_path(self._draft_template_path, doi_fields)
