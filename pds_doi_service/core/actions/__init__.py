a0a5faad6de2a29d05df99e322e7858bc939baaa
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-11-16 15:44:21 -0500

    change access to create_parser for sphynx doc after package reorganization

--- pds_doi_service/core/actions/__init__.py
+++ pds_doi_service/core/actions/__init__.py
@@ -3,3 +3,4 @@ from pds_doi_service.core.actions.check import DOICoreActionCheck
 from pds_doi_service.core.actions.draft import DOICoreActionDraft
 from pds_doi_service.core.actions.list import DOICoreActionList
 from pds_doi_service.core.actions.release import DOICoreActionRelease
+from pds_doi_service.core.actions.action import create_parser
