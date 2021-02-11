40d5bed5939f2125f94746bfdf4aa329cecd7e89
Scott Collins <collinss@jpl.nasa.gov>
2021-02-11 08:45:02 -0800

    Overhauled the input/input_util.py module to be the single location for functionality pertaining to reading of input files for DOI's
    There is now a single method, parse_dois_from_input_file(), that may be used to read DOI's from any of the supported extensions for both local and remote locations.
    The list of valid extensions may also be configured when creating a DOIInputUtil object.

--- pds_doi_service/core/input/input_util.py
+++ pds_doi_service/core/input/input_util.py
@@ -13,12 +13,22 @@ input_util.py
 Contains classes for working with input label files, be they local or remote.
 """
 
-import pandas as pd
+import os
+import urllib.parse
+import tempfile
+from os.path import basename
 from datetime import datetime
 
-from pds_doi_service.core.entities.doi import Doi, ProductType
+import pandas as pd
+import requests
+from lxml import etree
+
+from pds_doi_service.core.entities.doi import Doi, DoiStatus, ProductType
 from pds_doi_service.core.input.exceptions import InputFormatException
-from pds_doi_service.core.outputs.output_util import DOIOutputUtil
+from pds_doi_service.core.input.pds4_util import DOIPDS4LabelUtil
+from pds_doi_service.core.outputs.osti_web_parser import DOIOstiWebParser
+from pds_doi_service.core.util.config_parser import DOIConfigUtil
+from pds_doi_service.core.util.doi_validator import DOIValidator
 from pds_doi_service.core.util.general_util import get_logger
 
 # Get the common logger
@@ -26,18 +36,92 @@ logger = get_logger('pds_doi_service.core.input.input_util')
 
 
 class DOIInputUtil:
-    m_doi_output_util = DOIOutputUtil()
 
     EXPECTED_NUM_COLUMNS = 7
     """Expected number of columns in an input CSV file."""
 
     MANDATORY_COLUMNS = ['status', 'title', 'publication_date',
                          'product_type_specific', 'author_last_name',
-                         'author_first_name','related_resource']
+                         'author_first_name', 'related_resource']
     """The names of the expected columns within a CSV file."""
 
     EXPECTED_PUBLICATION_DATE_LEN = 10
     """Expected minimum length of a parsed publication date."""
+
+    DEFAULT_VALID_EXTENSIONS = ['.xml', '.csv', '.xlsx', '.xls']
+    """The default list of valid input file extensions this module can read."""
+
+    def __init__(self, valid_extensions=None):
+        """
+        Creates a new DOIInputUtil instance.
+
+        Parameters
+        ----------
+        valid_extensions : iterable, optional
+            A listing of the extensions this input util instance should
+            support. Must be a subset of the default extensions supported
+            by this module. If not provided, all the default extensions are
+            allowed.
+
+        Raises
+        ------
+        ValueError
+            If one or more unsupported extensions is provided.
+
+        """
+        self._config = DOIConfigUtil().get_config()
+        self._label_util = DOIPDS4LabelUtil(
+            landing_page_template=self._config.get('LANDING_PAGES', 'url')
+        )
+        self._valid_extensions = valid_extensions or self.DEFAULT_VALID_EXTENSIONS
+
+        if not isinstance(self._valid_extensions, (list, tuple, set)):
+            self._valid_extensions = [self._valid_extensions]
+
+        # Set up the mapping of supported extensions to the corresponding read
+        # function pointers
+        self._parser_map = {
+            '.xml': self.parse_xml_file,
+            '.xls': self.parse_sxls_file,
+            '.xlsx': self.parse_sxls_file,
+            '.csv': self.parse_csv_file
+        }
+
+        if not all([extension in self._parser_map
+                    for extension in self._valid_extensions]):
+            raise ValueError('One or more the provided extensions are not '
+                             'supported by the DOIInputUtil class.')
+
+    def parse_xml_file(self, xml_path):
+        """
+        Parses DOIs from a file with an .xml extension. The file is expected
+        to conform either to the PDS4 label or OSTI output label schema.
+
+        """
+        dois = []
+
+        # First read the contents of the file
+        with open(xml_path, 'r') as infile:
+            xml_contents = infile.read()
+
+        # Next, try validating xml against the OSTI schema to determine
+        # the format
+        if DOIValidator().validate_against_xsd(
+                xml_contents, use_alternate_validation_method=False):
+            dois, _ = DOIOstiWebParser.response_get_parse_osti_xml(xml_contents)
+        # Finally, try parsing as a PDS4 label
+        else:
+            try:
+                xml_tree = etree.fromstring(xml_contents.encode())
+
+                dois.append(self._label_util.get_doi_fields_from_pds4(xml_tree))
+            except Exception:
+                raise InputFormatException(
+                    f'Could not parse xml file {xml_path} as either a PDS4 label '
+                    f'or OSTI output label.'
+                )
+
+        return dois
 
     def parse_sxls_file(self, i_filepath):
         """
@@ -89,7 +173,6 @@ class DOIInputUtil:
         Given all rows in input file, parse each row and return the aggregated
         XML of all records in OSTI format.
         """
-
         doi_records = []
 
         for index, row in xl_sheet.iterrows():
@@ -118,7 +201,8 @@ class DOIInputUtil:
                     f"[{row['publication_date']}]"
                 )
 
-            doi = Doi(title=row['title'],
+            doi = Doi(status=DoiStatus(row['status'].lower()),
+                      title=row['title'],
                       publication_date=publication_date_value,
                       product_type=ProductType.Collection,
                       product_type_specific=row['product_type_specific'],
@@ -131,15 +215,13 @@ class DOIInputUtil:
 
         return doi_records
 
-    def parse_csv_file(self, i_filepath):
+    def parse_csv_file(self, csv_filepath):
         """
         Receives a URI containing CSV format and create one external file per
         row to output directory.
         """
-        logger.info("i_filepath " + i_filepath)
-
         # Read the CSV file into memory.
-        csv_sheet = pd.read_csv(i_filepath)
+        csv_sheet = pd.read_csv(csv_filepath)
         num_cols = len(csv_sheet.columns)
         num_rows = len(csv_sheet.index)
 
@@ -153,11 +235,154 @@ class DOIInputUtil:
                    f"CSV file, but only found {num_cols} columns.")
 
             logger.error(msg)
-            logger.error("i_filepath " + i_filepath)
+            logger.error("csv_filepath " + csv_filepath)
             logger.error("data columns " + str(list(csv_sheet.columns)))
             raise InputFormatException(msg)
         else:
             dois = self._parse_rows_to_doi_meta(csv_sheet)
             logger.info("FILE_WRITE_SUMMARY: num_rows " + str(num_rows))
+
+        return dois
+
+    def _read_from_path(self, path):
+        """
+        Parses Doi's from the file or files referenced by the provided path.
+        If the path points to a single file, only that file is parsed. Otherwise,
+        directories are walked for any files contained within. Any files with
+        unsupported extensions are ignored.
+
+        Parameters
+        ----------
+        path : str
+            Path to the location to read. May be a single file or directory.
+
+        Returns
+        -------
+        InputFormatException
+            If an error is encountered while reading a local file.
+
+        """
+        dois = []
+
+        if os.path.isfile(path):
+            logger.info(f'Reading local file path {path}')
+
+            extension = os.path.splitext(path)[-1]
+
+            if extension in self._valid_extensions:
+                # Select the appropriate read function based on the extension
+                read_function = self._parser_map[extension]
+
+                try:
+                    dois = read_function(path)
+                except OSError as err:
+                    msg = f'Error reading file {path}, reason: {str(err)}'
+
+                    logger.error(msg)
+                    raise InputFormatException(msg)
+            else:
+                logger.info(f'File {path} has unsupported extension, ignoring')
+        else:
+            logger.info(f'Reading files within directory {path}')
+
+            for sub_path in os.listdir(path):
+                dois.extend(self._read_from_path(os.path.join(path, sub_path)))
+
+        return dois
+
+    def _read_from_remote(self, input_url):
+        """
+        Reads a remote file from the provided URL into a local temporary file,
+        then parses and returns any Dois from it. The local temp file is
+        deleted once this function returns.
+
+        Parameters
+        ----------
+        input_url : str
+            The URL to the file to download locally.
+
+        Returns
+        -------
+        dois : list of Doi
+            The Doi's parsed from the remote file.
+
+        Raises
+        ------
+        InputFormatException
+            If the URL points to a file with an unsupported extension.
+
+        """
+        parsed_url = urllib.parse.urlparse(input_url)
+
+        # Check for valid extension before attempting to read from remote
+        extension = os.path.splitext(parsed_url.path)[-1]
+
+        if extension not in self._valid_extensions:
+            raise InputFormatException(
+                f'File extension type "{extension}" is not supported for this, '
+                f'operation, must be one of {",".join(self._valid_extensions)}'
+            )
+
+        response = requests.get(input_url)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as http_err:
+            raise InputFormatException(
+                f'Could not read remote file {input_url}, reason: {str(http_err)}'
+            )
+
+        with tempfile.NamedTemporaryFile(suffix=basename(parsed_url.path)) as temp_file:
+            temp_file.write(response.content)
+            temp_file.seek(0)
+
+            return self._read_from_path(temp_file.name)
+
+    def parse_dois_from_input_file(self, input_file):
+        """
+        Parses one or more Doi objects from the provided input file location.
+        The location may be a path to a local file or directory, or a remote
+        URL to the desired file.
+
+        Parameters
+        ----------
+        input_file : str
+            The location of the input to parse Doi's from. If a local directory,
+            the location will be walked for input files. If a remote URL,
+            the file will be read into a local temporary file, then processed
+            like a local path.
+
+        Returns
+        -------
+        dois : list of Doi
+            The list of Doi objects parsed from the input location.
+
+        Raises
+        ------
+        InputFormatException
+            If the input location does not correspond to a URL (starting with
+            http), or a local path, or if no Doi objects can be parsed from
+            the file (because it is an unsupported exception).
+
+        """
+        # See if we were handed a URL
+        if input_file.startswith('http'):
+            dois = self._read_from_remote(input_file)
+        # Otherwise see if its a local file
+        elif os.path.exists(input_file):
+            dois = self._read_from_path(input_file)
+        else:
+            raise InputFormatException(
+                f"Error reading file {input_file}, path does not correspond to "
+                f"a remote URL or a local file path."
+            )
+
+        # Make sure we got back at least one Doi
+        if not dois:
+            raise InputFormatException(
+                f"Unable to parse DOI's from input location {input_file}\n"
+                f"Please ensure the input is of the following type(s): "
+                f"{', '.join(self._valid_extensions)}"
+            )
 
         return dois
