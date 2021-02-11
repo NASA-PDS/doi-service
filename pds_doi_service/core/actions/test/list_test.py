89fa7274b0a0649381eeb0e308205e735ea8d486
Scott Collins <72415379+collinss-jpl@users.noreply.github.com>
2021-02-11 12:06:08 -0800

    List Action Filter by Status (#152)
    * Added command-line arg to expose workflow status as a query parameter in the list action
    
    * Rewrote list_test.py to test querying by status
    
    Additional unit tests for other query parameters will be added at a later date
    
    * Added status parameter to GET dois endpoint in swagger.yaml
    
    * Integrated use of status parameter to GET dois endpoint implementation in dois_controller.py
    
    * Added unit test for using status parameter with GET dois endpoint in test_dois_controller.py
    
    Co-authored-by: Scott Collins <collinss@jpl.nasa.gov>

--- pds_doi_service/core/actions/test/list_test.py
+++ pds_doi_service/core/actions/test/list_test.py
@@ -1,18 +1,110 @@
+#!/usr/bin/env python
+
+import json
+import os
+from os.path import abspath, dirname, join
 import unittest
+import tempfile
 
+from pds_doi_service.core.actions.draft import DOICoreActionDraft
 from pds_doi_service.core.actions.list import DOICoreActionList
+from pds_doi_service.core.actions.release import DOICoreActionRelease
+from pds_doi_service.core.entities.doi import DoiStatus
+from pds_doi_service.core.outputs.osti_web_parser import DOIOstiWebParser
 
-from pds_doi_service.core.util.general_util import get_logger
 
-logger = get_logger(__name__)
+class ListActionTestCase(unittest.TestCase):
+    # TODO: add additional unit tests for other list query parameters
 
-class MyTestCase(unittest.TestCase):
-    _action = DOICoreActionList()
+    def setUp(self):
+        self.test_dir = abspath(dirname(__file__))
+        self.input_dir = abspath(
+            join(self.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
+        )
+        self.db_name = join(self.test_dir, 'doi_temp.db')
+        self._list_action = DOICoreActionList(db_name=self.db_name)
+        self._draft_action = DOICoreActionDraft(db_name=self.db_name)
+        self._release_action = DOICoreActionRelease(db_name=self.db_name)
 
-    def test_1(self):
-        logger.info("test making a query to database")
-        result_list = self._action.run(node='img')
-        logger.info(result_list)
+    def tearDown(self):
+        if os.path.isfile(self.db_name):
+            os.remove(self.db_name)
+
+    def test_list_by_status(self):
+        """Test listing of entries, querying by workflow status"""
+        # Submit a draft, then query by draft status to retrieve
+        draft_kwargs = {
+            'input': join(self.input_dir, 'bundle_in_with_contributors.xml'),
+            'node': 'img',
+            'submitter': 'my_user@my_node.gov',
+            'force': True
+        }
+
+        draft_xml = self._draft_action.run(**draft_kwargs)
+
+        dois, _ = DOIOstiWebParser.response_get_parse_osti_xml(draft_xml)
+        doi = dois[0]
+
+        list_kwargs = {
+            'status': DoiStatus.Draft
+        }
+
+        list_result = json.loads(self._list_action.run(**list_kwargs))
+
+        self.assertEqual(len(list_result), 1)
+
+        list_result = list_result[0]
+        self.assertEqual(list_result['status'], doi.status)
+        self.assertEqual(list_result['title'], doi.title)
+        self.assertEqual(list_result['subtype'], doi.product_type_specific)
+        self.assertEqual(list_result['lid'] + '::' + list_result['vid'],
+                         doi.related_identifier)
+
+        # Now move the draft to review
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.xml') as temp_file:
+            temp_file.write(draft_xml)
+            temp_file.flush()
+
+            review_kwargs = {
+                'input': temp_file.name,
+                'node': 'img',
+                'submitter': 'my_user@my_node.gov',
+                'force': True,
+                'no_review': False
+            }
+
+            review_xml = self._release_action.run(**review_kwargs)
+
+        dois, _ = DOIOstiWebParser.response_get_parse_osti_xml(review_xml)
+        doi = dois[0]
+
+        # Now query for review status
+        list_kwargs = {
+            'status': DoiStatus.Review
+        }
+
+        list_result = json.loads(self._list_action.run(**list_kwargs))
+
+        self.assertEqual(len(list_result), 1)
+
+        list_result = list_result[0]
+        self.assertEqual(list_result['status'], doi.status)
+        self.assertEqual(list_result['title'], doi.title)
+        self.assertEqual(list_result['subtype'], doi.product_type_specific)
+        self.assertEqual(list_result['lid'] + '::' + list_result['vid'],
+                         doi.related_identifier)
+
+        # Finally, query for draft status again, should get no results back
+        list_kwargs = {
+            'status': DoiStatus.Draft
+        }
+
+        list_result = json.loads(self._list_action.run(**list_kwargs))
+
+        self.assertEqual(len(list_result), 0)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
