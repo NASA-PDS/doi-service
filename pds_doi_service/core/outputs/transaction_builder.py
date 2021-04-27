05727346c2d35ea52c60a1cb60b5099e9ca56650
Scott Collins <collinss@jpl.nasa.gov>
2021-04-27 15:49:41 -0700

    Refactored the Transaction and TransactionBuilder classes
    The TransactionBuilder class now queries the transaction database to ensure it has the most up-to-date information for the Doi objects it was provided.
    The updated Doi objects are then used by TransactionBuilder to create the output content label stored by Transaction.log() to the local filesystem.
    The output content can also now be fetched from a Transaction object via the output_content property.

--- pds_doi_service/core/outputs/transaction_builder.py
+++ pds_doi_service/core/outputs/transaction_builder.py
@@ -15,13 +15,13 @@ with the local database.
 """
 
 from pds_doi_service.core.db.doi_database import DOIDataBase
-from pds_doi_service.core.outputs.osti import CONTENT_TYPE_XML, VALID_CONTENT_TYPES
+from pds_doi_service.core.outputs.osti import DOIOutputOsti, CONTENT_TYPE_XML, VALID_CONTENT_TYPES
 from pds_doi_service.core.outputs.transaction import Transaction
-from pds_doi_service.core.outputs.transaction_on_disk import TransactionOnDisk
+
 from pds_doi_service.core.util.config_parser import DOIConfigUtil
 from pds_doi_service.core.util.general_util import get_logger
 
-logger = get_logger('pds_doi_service.core.outputs.transaction_builder')
+logger = get_logger(__name__)
 
 
 class TransactionBuilder:
@@ -31,35 +31,16 @@ class TransactionBuilder:
     """
     m_doi_config_util = DOIConfigUtil()
 
-    # A database writer to write DOI info to table.
-    m_doi_database = None
-    # A logger to write transaction to disk and to database.
-    m_transaction_ondisk_dao = None
-    # A transaction contains list of dictionaries containing fields to write
-    # to disk and database.
-    m_transaction = None
-
-    def __init__(self,db_name=None):
+    def __init__(self, db_name=None):
         self._config = self.m_doi_config_util.get_config()
+
         if db_name:
             self.m_doi_database = DOIDataBase(db_name)
         else:
             self.m_doi_database = DOIDataBase(self._config.get('OTHER', 'db_file'))
 
-        self.m_transaction_ondisk_dao = TransactionOnDisk()
-
-    def get_transaction(self):
-        return self.m_transaction
-
-    def get_transaction_logger(self):
-        return self.m_transaction_ondisk_dao
-
-    def get_doi_database_writer(self):
-        return self.m_doi_database
-
     def prepare_transaction(self, node_id, submitter_email, dois: list,
-                            input_path=None, output_content=None,
-                            output_content_type=CONTENT_TYPE_XML):
+                            input_path=None, output_content_type=CONTENT_TYPE_XML):
         """
         Build a Transaction from the inputs and outputs to a 'reserve', 'draft'
         or release action. The Transaction object is returned.
@@ -72,13 +53,38 @@ class TransactionBuilder:
             raise ValueError('Invalid content type requested, must be one of '
                              f'{",".join(VALID_CONTENT_TYPES)}')
 
+        for doi in dois:
+            lidvid = doi.related_identifier
+            lid, vid = lidvid.split('::')
+
+            # Get the latest available entry in the DB for this lidvid, if it exists
+            query_criteria = {'lidvid': [lidvid]} if vid else {'lid': [lid]}
+            columns, rows = self.m_doi_database.select_latest_rows(query_criteria)
+
+            # Get the latest transaction record for this LIDVID so we can carry
+            # forward certain fields to the next transaction
+            if rows:
+                latest_row = dict(zip(columns, rows[0]))
+
+                # Carry original release date forward
+                doi.date_record_added = latest_row['release_date']
+
+                # We might have a DOI already in the database from a previous reserve
+                if not doi.doi and latest_row['doi']:
+                    doi.doi = latest_row['doi']
+                    doi.id = doi.doi.split('/')[-1]
+
+        # Create the output label that's written to the local transaction
+        # history on disk. This label should represent the most up-to-date
+        # version for this DOI/LIDVID
+        output_content = DOIOutputOsti().create_osti_doi_record(
+            dois, content_type=output_content_type
+        )
+
         return Transaction(output_content,
                            output_content_type,
                            node_id,
                            submitter_email,
                            dois,
-                           self.m_transaction_ondisk_dao,
                            self.m_doi_database,
                            input_path=input_path)
-
-# end class TransactionBuilder
