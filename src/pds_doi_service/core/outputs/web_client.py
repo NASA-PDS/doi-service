9e0513851e37d04b9517a4fe207c4eaf5be2bf9a
Scott Collins <collinss@jpl.nasa.gov>
2021-06-03 12:33:17 -0700

    Added a handle to DOIConfigUtil() to the DOIWebClient class
    This gives inheritors a means to acquire the service specific credentials/configurations to feed to the base _submit_content function.
    This in turn allowed the interface for submit_content and query_doi to be simplified.

--- src/pds_doi_service/core/outputs/web_client.py
+++ src/pds_doi_service/core/outputs/web_client.py
@@ -21,16 +21,18 @@ from requests.auth import HTTPBasicAuth
 
 from pds_doi_service.core.input.exceptions import WebRequestException
 from pds_doi_service.core.outputs.doi_record import CONTENT_TYPE_XML
+from pds_doi_service.core.util.config_parser import DOIConfigUtil
 
 
 class DOIWebClient:
     """Abstract base class for clients of an HTTP DOI service endpoint"""
+    _config_util = DOIConfigUtil()
     _service_name = None
     _web_parser = None
     _content_type_map = {}
 
-    def submit_content(self, payload, url, username, password,
-                       content_type=CONTENT_TYPE_XML):
+    def _submit_content(self, payload, url, username, password,
+                        content_type=CONTENT_TYPE_XML):
         """
         Submits a payload to a DOI service endpoint via the POST action.
 
@@ -87,22 +89,52 @@ class DOIWebClient:
 
         return response.text
 
-    def query_doi(self, url, query, username, password,
-                  content_type=CONTENT_TYPE_XML):
+    def submit_content(self, payload, content_type=CONTENT_TYPE_XML):
         """
-        Queries the DOI endpoint for the status of one or more DOI submissions.
+        Submits the provided payload to a DOI service endpoint via the POST
+        action.
+
+        Inheritors of DOIWebClient should pull any required endpoint specific
+        parameters (URL, username, password, etc...) from the configuration
+        util bundled with the class.
+
+        Inheritors should also take the extra step of parsing and returning any
+        Doi objects from the response text (if request was successful).
 
         Parameters
         ----------
-        url : str
-             The URL of the DOI service endpoint.
+        payload : str
+            Payload to submit to the DOI service.
+        content_type : str
+            The content type to specify the format of the payload, as well as
+            the format of the response from the endpoint.
+
+        Returns
+        -------
+        dois : list of Doi
+            Doi objects parsed from the response text.
+        response_text : str
+            Body of the response text.
+
+        """
+        raise NotImplementedError(
+            f'Subclasses of {self.__class__.__name__} must provide an '
+            f'implementation for submit_content()'
+        )
+
+    def query_doi(self, query, content_type=CONTENT_TYPE_XML):
+        """
+        Queries the DOI endpoint for the status of one or more DOI submissions.
+
+        Inheritors of DOIWebClient should pull any required endpoint specific
+        parameters (URL, username, password, etc...) from the configuration
+        util bundled with the class.
+
+        Parameters
+        ----------
         query : dict
             Key/value pairs to append as parameters to the URL for the GET
             endpoint.
-        username : str
-            The user name to authenticate to the DOI service as.
-        password : str
-            The password to authenticate to the DOI service with.
         content_type : str
             The content type to specify the the format of the response from the
             endpoint.
@@ -114,6 +146,6 @@ class DOIWebClient:
 
         """
         raise NotImplementedError(
-            'Subclasses of DOIWebClient must provide an implementation for '
-            'query_doi()'
+            f'Subclasses of {self.__class__.__name__} must provide an '
+            f'implementation for query_doi()'
         )
