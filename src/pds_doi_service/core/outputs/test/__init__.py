eec0da02526f0549b5357858f298c30cbda63aac
Scott Collins <collinss@jpl.nasa.gov>
2021-06-29 15:02:16 -0700

    Added unit tests for DOIDataCiteWebClient and DOIDataCiteRecord

--- src/pds_doi_service/core/outputs/test/__init__.py
+++ src/pds_doi_service/core/outputs/test/__init__.py
@@ -6,12 +6,14 @@ Planetary Data System's Digital Object Identifier service — tests for core out
 
 
 import unittest
+from . import datacite_test
 from . import doi_validator_test
 from . import osti_test
 
 
 def suite():
     suite = unittest.TestSuite()
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(datacite_test))
     suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(doi_validator_test))
     suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(osti_test))
     return suite
--- src/pds_doi_service/core/outputs/test/datacite_test.py
+++ src/pds_doi_service/core/outputs/test/datacite_test.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 import os
+import json
+import requests
+import unittest
 from datetime import datetime
 from os.path import abspath, join
-import unittest
+from requests.models import Response
+from unittest.mock import patch
 
 from pkg_resources import resource_filename
 
-from pds_doi_service.core.entities.doi import ProductType, DoiStatus
+from pds_doi_service.core.entities.doi import ProductType, Doi, DoiStatus
+from pds_doi_service.core.outputs.datacite.datacite_web_client import DOIDataCiteWebClient
 from pds_doi_service.core.outputs.datacite.datacite_record import DOIDataCiteRecord
 from pds_doi_service.core.outputs.datacite.datacite_web_parser import DOIDataCiteWebParser
 
@@ -19,7 +24,7 @@ class DOIDataCiteRecordTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.test_dir = resource_filename(__name__, '')
         cls.input_dir = abspath(
-            join(cls.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
+            join(cls.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
         )
 
     def test_create_datacite_label_json(self):
@@ -43,15 +48,126 @@ class DOIDataCiteRecordTestCase(unittest.TestCase):
 
         self.assertDictEqual(input_doi_fields, output_doi_fields)
 
+    def test_assign_datacite_event(self):
+        """
+        Test assignment of the event field when creating a label for a DOI
+        in the reserved or pending state
+        """
+        # Create a dummy Doi object to be reserved
+        test_doi = Doi(title='InSight Cameras Bundle',
+                       publication_date=datetime(2019, 1, 1, 0, 0),
+                       product_type=ProductType.Dataset,
+                       product_type_specific='PDS4 Refereed Data Bundle',
+                       related_identifier='urn:nasa:pds:insight_cameras::1.0',
+                       id='yzw2-vz66',
+                       doi='10.13143/yzw2-vz66',
+                       publisher='NASA Planetary Data System',
+                       contributor='Engineering',
+                       status=DoiStatus.Reserved)
 
-class DOIODataCiteWebParserTestCase(unittest.TestCase):
+        # Create the label to submit to DataCite, since the Doi has been set
+        # to reserve, the label should contain the "register" event
+        reserve_label = DOIDataCiteRecord().create_doi_record(test_doi)
+        reserve_label_dict = json.loads(reserve_label)
+
+        self.assertIn('event', reserve_label_dict['data']['attributes'])
+        self.assertEqual(reserve_label_dict['data']['attributes']['event'], 'register')
+
+        # Now test with the Pending (release) state, which should map to the
+        # "publish" event
+        test_doi.status = DoiStatus.Pending
+
+        release_label = DOIDataCiteRecord().create_doi_record(test_doi)
+        release_label_dict = json.loads(release_label)
+
+        self.assertIn('event', release_label_dict['data']['attributes'])
+        self.assertEqual(release_label_dict['data']['attributes']['event'], 'publish')
+
+        # For any other state, we should not get an event field in the label
+        test_doi.status = DoiStatus.Reserved_not_submitted
+
+        reserve_label = DOIDataCiteRecord().create_doi_record(test_doi)
+        reserve_label_dict = json.loads(reserve_label)
+
+        self.assertNotIn('event', reserve_label_dict['data']['attributes'])
+
+
+def requests_valid_request_patch(method, url, **kwargs):
+    response = Response()
+    response.status_code = 200
+
+    with open(join(DOIDataCiteWebClientTestCase.input_dir,
+                   'DOI_Release_20210615_from_release.json')) as infile:
+        response._content = infile.read().encode()
+
+    return response
+
+
+class DOIDataCiteWebClientTestCase(unittest.TestCase):
+    """Unit tests for the datacite_web_client.py module"""
+    input_dir = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_dir = resource_filename(__name__, '')
+        cls.input_dir = abspath(
+            join(cls.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
+        )
+
+    @patch.object(requests, 'request', requests_valid_request_patch)
+    def test_submit_content(self):
+        """Test the datacite_web_client.submit_content method"""
+        test_doi = Doi(title='InSight Cameras Bundle',
+                       publication_date=datetime(2019, 1, 1, 0, 0),
+                       product_type=ProductType.Dataset,
+                       product_type_specific='PDS4 Refereed Data Bundle',
+                       related_identifier='urn:nasa:pds:insight_cameras::1.0',
+                       id='yzw2-vz66',
+                       doi='10.13143/yzw2-vz66',
+                       publisher='NASA Planetary Data System',
+                       contributor='Engineering',
+                       status=DoiStatus.Reserved)
+
+        test_payload = DOIDataCiteRecord().create_doi_record(test_doi)
+
+        response_doi, response_text = DOIDataCiteWebClient().submit_content(test_payload)
+
+        # Ensure the response DOI and text line up
+        response_text_doi, _ = DOIDataCiteWebParser.parse_dois_from_label(response_text)
+        self.assertEqual(response_doi, response_text_doi[0])
+
+        # Ensure the DOI returned corresponds to the one we provided
+        self.assertEqual(test_doi.title, response_doi.title)
+        self.assertEqual(test_doi.related_identifier, response_doi.related_identifier)
+        self.assertEqual(test_doi.doi, response_doi.doi)
+
+        # Check that the status has been updated by the submission request
+        self.assertEqual(response_doi.status, DoiStatus.Findable)
+
+    @patch.object(requests, 'request', requests_valid_request_patch)
+    def test_query_doi(self):
+        """Test the datacite_web_client.query_doi method"""
+        # Test with a single query term and a query dictionary
+        queries = ('PDS', {'id': '10.13143/yzw2-vz66'})
+
+        for query in queries:
+            response_text = DOIDataCiteWebClient().query_doi(query)
+
+            response_dois, _ = DOIDataCiteWebParser.parse_dois_from_label(response_text)
+            response_doi = response_dois[0]
+
+            # Should get the same record back for both queries
+            self.assertEqual(response_doi.doi, '10.13143/yzw2-vz66')
+
+
+class DOIDataCiteWebParserTestCase(unittest.TestCase):
     """Unit tests for the datacite_web_parser.py module"""
 
     @classmethod
     def setUpClass(cls):
         cls.test_dir = resource_filename(__name__, '')
         cls.input_dir = abspath(
-            join(cls.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
+            join(cls.test_dir, os.pardir, os.pardir, os.pardir, os.pardir, os.pardir, 'input')
         )
 
         cls.expected_authors = [{'first_name': 'R.', 'last_name': 'Deen'},
