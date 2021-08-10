6fb3a454dcf60b9912e8005e2a938fefc54efd5c
Scott Collins <collinss@jpl.nasa.gov>
2021-08-10 10:14:17 -0700

    Overhauled the doi_database.py module to more easily support updates to the DOI table schema.
    The schema definition is now defined in a single constant, DOI_DB_SCHEMA, from which all other CRUD functions take their cue.
    The schema has also been updated to rename the lid column to "identifier", and the vid column has been removed.
    Lastly, the release_date/update_date columns have been renamed to date_added/date_updated for consistency with the rest of the code base.

--- src/pds_doi_service/core/db/test/doi_database_test.py
+++ src/pds_doi_service/core/db/test/doi_database_test.py
@@ -26,7 +26,7 @@ class DOIDatabaseTest(unittest.TestCase):
         if exists(self._db_name):
             os.remove(self._db_name)
 
-        self._doi_database = DOIDataBase('doi_temp.db')
+        self._doi_database = DOIDataBase(self._db_name)
 
     def tearDown(self):
         if exists(self._db_name):
@@ -35,12 +35,11 @@ class DOIDatabaseTest(unittest.TestCase):
     def test_select_latest_rows(self):
         """Test selecting of latest rows from the transaction database"""
         # Set up a sample db entry
-        lid = 'urn:nasa:pds:lab_shocked_feldspars'
-        vid = '1.0'
+        identifier = 'urn:nasa:pds:lab_shocked_feldspars::1.0'
         transaction_key = 'img/2020-06-15T18:42:45.653317'
         doi = '10.17189/21729'
-        release_date = datetime.datetime.now()
-        transaction_date = datetime.datetime.now()
+        date_added = datetime.datetime.now()
+        date_updated = datetime.datetime.now()
         status = DoiStatus.Unknown
         title = 'Laboratory Shocked Feldspars Bundle'
         product_type = ProductType.Collection
@@ -50,7 +49,7 @@ class DOIDatabaseTest(unittest.TestCase):
 
         # Insert a row in the 'doi' table
         self._doi_database.write_doi_info_to_database(
-            lid, vid, transaction_key, doi, release_date, transaction_date, status,
+            identifier, transaction_key, doi, date_added, date_updated, status,
             title, product_type, product_type_specific, submitter, discipline_node
         )
 
@@ -66,20 +65,19 @@ class DOIDatabaseTest(unittest.TestCase):
         # Ensure we got back everything we just put in
         self.assertEqual(query_result['status'], status)
         self.assertEqual(
-            int(query_result['release_date'].timestamp()),
-            int(release_date.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
+            int(query_result['date_added'].timestamp()),
+            int(date_added.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
         )
         self.assertEqual(
-            int(query_result['update_date'].timestamp()),
-            int(transaction_date.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
+            int(query_result['date_updated'].timestamp()),
+            int(date_updated.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
         )
         self.assertEqual(query_result['submitter'], submitter)
         self.assertEqual(query_result['title'], title)
         self.assertEqual(query_result['type'], product_type)
         self.assertEqual(query_result['subtype'], product_type_specific)
         self.assertEqual(query_result['node_id'], discipline_node)
-        self.assertEqual(query_result['lid'], lid)
-        self.assertEqual(query_result['vid'], vid)
+        self.assertEqual(query_result['identifier'], identifier)
         self.assertEqual(query_result['doi'], doi)
         self.assertEqual(query_result['transaction_key'], transaction_key)
 
@@ -91,7 +89,7 @@ class DOIDatabaseTest(unittest.TestCase):
         discipline_node = 'eng'
 
         self._doi_database.write_doi_info_to_database(
-            lid, vid, transaction_key, doi, release_date, transaction_date, status,
+            identifier, transaction_key, doi, date_added, date_updated, status,
             title, product_type, product_type_specific, submitter, discipline_node
         )
 
@@ -116,12 +114,11 @@ class DOIDatabaseTest(unittest.TestCase):
     def test_select_latest_rows_lid_only(self):
         """Test corner case where we select and update rows that only specify a LID"""
         # Set up a sample db entry
-        lid = 'urn:nasa:pds:insight_cameras'
-        vid = None
+        identifier = 'urn:nasa:pds:insight_cameras'
         transaction_key = 'img/2021-05-10T00:00:00.000000'
         doi = '10.17189/22000'
-        release_date = datetime.datetime.now()
-        transaction_date = datetime.datetime.now()
+        date_added = datetime.datetime.now()
+        date_updated = datetime.datetime.now()
         status = DoiStatus.Unknown
         title = 'Insight Cameras Bundle'
         product_type = ProductType.Collection
@@ -131,14 +128,14 @@ class DOIDatabaseTest(unittest.TestCase):
 
         # Insert a row in the 'doi' table
         self._doi_database.write_doi_info_to_database(
-            lid, vid, transaction_key, doi, release_date, transaction_date, status,
+            identifier, transaction_key, doi, date_added, date_updated, status,
             title, product_type, product_type_specific, submitter, discipline_node
         )
 
         # Select the row we just added
         # The type of o_query_result should be JSON and a list of 1
         o_query_result = self._doi_database.select_latest_rows(
-            query_criterias={'lidvid': [lid]}
+            query_criterias={'ids': [identifier]}
         )
 
         # Reformat results to a dictionary to test with
@@ -147,20 +144,19 @@ class DOIDatabaseTest(unittest.TestCase):
         # Ensure we got back everything we just put in
         self.assertEqual(query_result['status'], status)
         self.assertEqual(
-            int(query_result['release_date'].timestamp()),
-            int(release_date.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
+            int(query_result['date_added'].timestamp()),
+            int(date_added.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
         )
         self.assertEqual(
-            int(query_result['update_date'].timestamp()),
-            int(transaction_date.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
+            int(query_result['date_updated'].timestamp()),
+            int(date_updated.replace(tzinfo=timezone(timedelta(hours=--8.0))).timestamp())
         )
         self.assertEqual(query_result['submitter'], submitter)
         self.assertEqual(query_result['title'], title)
         self.assertEqual(query_result['type'], product_type)
         self.assertEqual(query_result['subtype'], product_type_specific)
         self.assertEqual(query_result['node_id'], discipline_node)
-        self.assertEqual(query_result['lid'], lid)
-        self.assertEqual(query_result['vid'], vid)
+        self.assertEqual(query_result['identifier'], identifier)
         self.assertEqual(query_result['doi'], doi)
         self.assertEqual(query_result['transaction_key'], transaction_key)
 
@@ -172,13 +168,13 @@ class DOIDatabaseTest(unittest.TestCase):
         discipline_node = 'img'
 
         self._doi_database.write_doi_info_to_database(
-            lid, vid, transaction_key, doi, release_date, transaction_date, status,
+            identifier, transaction_key, doi, date_added, date_updated, status,
             title, product_type, product_type_specific, submitter, discipline_node
         )
 
         # Query again and ensure we only get latest back
         o_query_result = self._doi_database.select_latest_rows(
-            query_criterias={'lidvid': [lid]}
+            query_criterias={'ids': [identifier]}
         )
 
         # Should only get the one row back
@@ -205,10 +201,11 @@ class DOIDatabaseTest(unittest.TestCase):
         for _id in range(1, 1 + num_rows):
             lid = 'urn:nasa:pds:lab_shocked_feldspars'
             vid = f'{_id}.0'
+            identifier = lid + '::' + vid
             transaction_key = f'img/{_id}/2020-06-15T18:42:45.653317'
             doi = f'10.17189/2000{_id}'
-            release_date = datetime.datetime.now()
-            transaction_date = datetime.datetime.now()
+            date_added = datetime.datetime.now()
+            date_updated = datetime.datetime.now()
             status = DoiStatus.Draft
             title = f'Laboratory Shocked Feldspars Bundle {_id}'
             product_type = ProductType.Collection
@@ -217,30 +214,30 @@ class DOIDatabaseTest(unittest.TestCase):
             discipline_node = 'img'
 
             self._doi_database.write_doi_info_to_database(
-                lid, vid, transaction_key, doi, release_date, transaction_date, status,
+                identifier, transaction_key, doi, date_added, date_updated, status,
                 title, product_type, product_type_specific, submitter, discipline_node
             )
 
         # Use a wildcard with lidvid column to select everything we just
         # inserted
         o_query_result = self._doi_database.select_latest_rows(
-            query_criterias={'lidvid': ['urn:nasa:pds:lab_shocked_feldspars::*']}
+            query_criterias={'ids': ['urn:nasa:pds:lab_shocked_feldspars::*']}
         )
 
         # Should get all rows back
         self.assertEqual(len(o_query_result[-1]), num_rows)
 
-        # Try again using just the lid column
+        # Try again using just the lid
         o_query_result = self._doi_database.select_latest_rows(
-            query_criterias={'lid': ['urn:nasa:*:lab_shocked_feldspars']}
+            query_criterias={'ids': ['urn:nasa:*:lab_shocked_feldspars::1.0']}
         )
 
-        self.assertEqual(len(o_query_result[-1]), num_rows)
+        self.assertEqual(len(o_query_result[-1]), 1)
 
         # Test with a combination of wildcards and full tokens
         o_query_result = self._doi_database.select_latest_rows(
-            query_criterias={'lidvid': ['urn:nasa:pds:lab_shocked_feldspars::1.0',
-                                        'urn:nasa:pds:lab_shocked_feldspars::2.*']}
+            query_criterias={'ids': ['urn:nasa:pds:lab_shocked_feldspars::1.0',
+                                     'urn:nasa:pds:lab_shocked_feldspars::2.*']}
         )
 
         self.assertEqual(len(o_query_result[-1]), 2)
