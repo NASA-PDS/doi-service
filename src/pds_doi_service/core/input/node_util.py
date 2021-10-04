6418d493104a85a7207bfdd4798b4e0940572323
Scott Collins <collinss@jpl.nasa.gov>
2021-10-04 16:16:44 -0700

    Added Radio Science to list of PDS nodes in node_util.py

--- src/pds_doi_service/core/input/node_util.py
+++ src/pds_doi_service/core/input/node_util.py
@@ -1,16 +1,15 @@
-#!/bin/python
 #
 #  Copyright 2020, by the California Institute of Technology.  ALL RIGHTS
 #  RESERVED. United States Government Sponsorship acknowledged. Any commercial
 #  use must be negotiated with the Office of Technology Transfer at the
 #  California Institute of Technology.
 #
-# ------------------------------
+
 from pds_doi_service.core.input.exceptions import UnknownNodeException
 from pds_doi_service.core.util.general_util import get_logger
 
 # Get the common logger and set the level for this file.
-logger = get_logger("pds_doi_core.input.node_util")
+logger = get_logger(__name__)
 
 
 class NodeUtil:
@@ -23,6 +22,7 @@ class NodeUtil:
         "IMG": "Cartography and Imaging Sciences Discipline",
         "NAIF": "Navigational and Ancillary Information Facility",
         "PPI": "Planetary Plasma Interactions",
+        "RS": "Radio Science",
         "RMS": "Ring-Moon Systems",
         "SBN": "Small Bodies",
     }
@@ -40,8 +40,3 @@ class NodeUtil:
     @classmethod
     def get_permissible_values(cls):
         return [c.lower() for c in cls.m_node_id_dict.keys()]
-
-
-#    @classmethod
-#    def get_keys(cls):
-#        return [c.lower() for c in cls.m_node_id_dict.keys()]
