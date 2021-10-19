918a721517637bb562d261c80d723b2be19e29fd
Scott Collins <collinss@jpl.nasa.gov>
2021-10-19 11:21:54 -0700

    Merged contents of the references package into the util package

--- src/pds_doi_service/core/util/test/__init__.py
+++ src/pds_doi_service/core/util/test/__init__.py
@@ -5,11 +5,13 @@ Planetary Data System's Digital Object Identifier service — tests for core uti
 import unittest
 
 from . import config_parser_test
+from . import contributors_util_test
 from . import general_util_test
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(config_parser_test))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(contributors_util_test))
     suite.addTests(unittest.defaultTestLoader.loadTestsFromModule(general_util_test))
     return suite
--- src/pds_doi_service/test.py
+++ src/pds_doi_service/test.py
@@ -9,17 +9,15 @@ import pds_doi_service.core.actions.test
 import pds_doi_service.core.db.test
 import pds_doi_service.core.input.test
 import pds_doi_service.core.outputs.test
-import pds_doi_service.core.references.test
 import pds_doi_service.core.util.test
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTests(pds_doi_service.core.util.test.suite())
-    suite.addTests(pds_doi_service.core.input.test.suite())
-    suite.addTests(pds_doi_service.core.references.test.suite())
     suite.addTests(pds_doi_service.core.actions.test.suite())
     suite.addTests(pds_doi_service.core.db.test.suite())
+    suite.addTests(pds_doi_service.core.input.test.suite())
     suite.addTests(pds_doi_service.core.outputs.test.suite())
+    suite.addTests(pds_doi_service.core.util.test.suite())
     suite.addTests(pds_doi_service.api.test.suite())
     return suite
