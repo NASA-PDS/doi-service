ab1bc878fe505a8ad46bf2140379da4735f19e44
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2022-08-03 22:45:14 -0700

    add cognito (jwt bearer) authentication

---
+++ src/pds_doi_service/api/controllers/authentication.py
@@ -0,0 +1,31 @@
+import time
+import logging
+from jose import JWTError, jwt
+from flask import current_app
+from werkzeug.exceptions import Unauthorized
+from pds_doi_service.core.util.config_parser import DOIConfigUtil
+
+config = DOIConfigUtil().get_config()
+
+JWT_ISSUER = config.get('API_AUTHENTICATION', 'jwt_issuer')
+JWT_SECRET = config.get('API_AUTHENTICATION', 'jwt_secret')
+JWT_LIFETIME_SECONDS = 600
+JWT_ALGORITHM = "RS256"
+
+
+def decode_token(token):
+    try:
+        current_app.logger.debug("try to decode/validate token %s", token)
+        return jwt.decode(
+            token,
+            JWT_SECRET,
+            algorithms=[JWT_ALGORITHM],
+            issuer=JWT_ISSUER,
+            options={
+                'verify_signature': False,
+                'verify_iss': True
+            }
+        )
+    except JWTError as e:
+        current_app.logger.error("authentication exception")
+        raise Unauthorized from e
