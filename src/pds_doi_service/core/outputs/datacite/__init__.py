8b93dfb5d9b4d5b89fc82d7c4f34f4e67a4db80b
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-06-03 14:34:28 -0700

    Cleanup per black run

--- src/pds_doi_service/core/outputs/datacite/__init__.py
+++ src/pds_doi_service/core/outputs/datacite/__init__.py
@@ -6,6 +6,7 @@ DataCite
 This package contains the DataCite-specific implementations for the abstract
 classes of the outputs package.
 """
+
 from .datacite_record import DOIDataCiteRecord  # noqa: F401
 from .datacite_validator import DOIDataCiteValidator  # noqa: F401
 from .datacite_web_client import DOIDataCiteWebClient  # noqa: F401
