8e723c4e8b79dcdc1c46d3c6c38b7bcf719749fe
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-06-03 15:27:50 -0700

    Final attempts to get tests and linter to work

--- src/pds_doi_service/core/outputs/datacite/__init__.py
+++ src/pds_doi_service/core/outputs/datacite/__init__.py
@@ -7,6 +7,7 @@ This package contains the DataCite-specific implementations for the abstract
 classes of the outputs package.
 """
 
+# noreorder
 from .datacite_record import DOIDataCiteRecord  # noqa: F401
 from .datacite_validator import DOIDataCiteValidator  # noqa: F401
 from .datacite_web_client import DOIDataCiteWebClient  # noqa: F401
