c391d7bedb95ca17f69531f5f58503168a218b2c
Jordan Padams <33492486+jordanpadams@users.noreply.github.com>
2025-10-25 08:47:05 -0700

    Update review_email.sh

--- scripts/review_email.sh
+++ scripts/review_email.sh
@@ -4,7 +4,7 @@ echo "=================================================="
 echo "pdscloud-gamma: Starting DOI review email $(date)"
 echo
 
-source /home/pds4/pds-doi-service/bin/activate
+source $HOME/pds-doi-service/bin/activate
 
 pds-doi-cmd list --status review | mail -s 'DOI daily review on pdscloud-gamma' pdsen-operator@jpl.nasa.gov,ronald.joyner@jpl.nasa.gov
 
