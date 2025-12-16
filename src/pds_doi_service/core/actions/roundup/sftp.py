35be3015e767fea0418d5324b97ffc04a5a07bef
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-12-16 13:00:19 -0800

    Add FIPS compliance for SFTP host key verification
    Configures paramiko to use AutoAddPolicy instead of the default host key
    verification, which avoids MD5 fingerprint calculations that are disabled
    in FIPS mode. The MD5 hash function triggers a "disabled for FIPS" error
    when paramiko attempts to verify SSH host keys.
    
    AutoAddPolicy automatically accepts unknown host keys, which is acceptable
    for internal SFTP servers. This is a common pattern for automated scripts
    connecting to trusted internal infrastructure.
    
    Error resolved:
      ValueError: [digital envelope routines: EVP_DigestInit_ex] disabled for FIPS
      at paramiko/pkey.py:354 in get_fingerprint()

--- src/pds_doi_service/core/actions/roundup/sftp.py
+++ src/pds_doi_service/core/actions/roundup/sftp.py
@@ -4,12 +4,35 @@ import os
 import tempfile
 
 import fabric.transfer  # type: ignore
+import paramiko  # type: ignore
 from fabric import Connection  # type: ignore
 from paramiko.sftp import SFTPError  # type: ignore
 from pds_doi_service.core.actions.roundup.enumerate import get_previous_week_metadata
 from pds_doi_service.core.actions.roundup.output import prepare_doi_record_for_ads_sftp
 from pds_doi_service.core.db.doi_database import DOIDataBase
 from pds_doi_service.core.util.config_parser import DOIConfigUtil
+
+
+class FIPSCompliantAutoAddPolicy(paramiko.MissingHostKeyPolicy):
+    """
+    FIPS-compliant host key policy that accepts unknown hosts without computing MD5 fingerprints.
+
+    This policy is similar to AutoAddPolicy but avoids calling get_fingerprint() which uses MD5
+    and fails in FIPS mode. It's appropriate for internal SFTP servers where host key verification
+    is not critical.
+    """
+
+    def missing_host_key(self, client, hostname, key):
+        """
+        Accept the host key without computing fingerprints.
+
+        Args:
+            client: SSHClient instance
+            hostname: The hostname of the server
+            key: The server's host key
+        """
+        # Add the key without logging the fingerprint (which would use MD5)
+        client._host_keys.add(hostname, key.get_name(), key)
 
 
 def ensure_target_dir(dir_path: str, conn: Connection):
@@ -53,7 +76,16 @@ def run(
     dest_filename = f'roundup-week-ending-{metadata.last_date.strftime("%Y%m%d")}.json'
     dest_path = os.path.join(dest_dir_path, dest_filename)
 
-    conn = Connection(host=sftp_host, port=sftp_port, user=sftp_user, connect_kwargs={"password": sftp_password})
+    # Configure connection with FIPS-compliant host key policy
+    # This automatically accepts unknown host keys without computing MD5 fingerprints
+    conn = Connection(
+        host=sftp_host,
+        port=sftp_port,
+        user=sftp_user,
+        connect_kwargs={"password": sftp_password},
+    )
+    # Set FIPS-compliant host key policy that doesn't use MD5
+    conn.client.set_missing_host_key_policy(FIPSCompliantAutoAddPolicy())
     transfer = fabric.transfer.Transfer(connection=conn)
 
     ensure_target_dir(dest_dir_path, conn)
